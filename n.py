2292f59dc7e7d8bf93ee0a38bf099943713b7bc5
VLADIS-py <62959852+VLADIS-py@users.noreply.github.com>
2020-05-02 03:08:15 +0000

    Create n.py

---
+++ n.py
@@ -0,0 +1,78 @@
+import urllib
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   HEADER = '\033[95m'
+   OKBLUE = '\033[94m'
+   OKGREEN = '\033[92m'
+   WARNING = '\033[93m'
+   FAIL = '\033[91m'
+
+print (color.END +"Введите корень: ")
+lol = input(color.GREEN +"math-$>> ")
+if lol ==	"4":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("2")
+if lol ==	"9":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("3")
+if lol ==	"16":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("4")
+if lol ==	"25":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("5")
+if lol ==	"36":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("6")
+if lol ==	"49":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("7")
+if lol ==	"64":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("8")
+if lol ==	"81":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("9")
+if lol ==	"100":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("10")
+if lol ==	"121":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("11")
+if lol ==	"144":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("12")
+if lol ==	"169":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("13")
+if lol ==	"196":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("14")
+if lol ==	"225":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("15")
+if lol ==	"256":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("16")
+if lol ==	"289":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("17")
+if lol ==	"324":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("18")
+if lol ==	"361":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("19")
+if lol ==	"400":
+	print (color.END +"\nВаш ответ равен:\n")
+	print ("20")
